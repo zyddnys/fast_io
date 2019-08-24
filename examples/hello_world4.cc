8d23b23c3ca65a880014566f1706750a5815afbb
ejsvifq_mabmip <euloanty@live.com>
2019-08-24 16:35:39 -0400

    w

---
+++ examples/hello_world4.cc
@@ -0,0 +1,6 @@
+#include"../include/fast_io.h"
+
+int main()
+{
+	fast_io::system_out<<"Hello World\n";
+}
--- include/fast_io.h
+++ include/fast_io.h
@@ -17,38 +17,38 @@
 
 namespace fast_io
 {
-	#ifdef _WIN32_WINNT
-		using system_io_handle = c_style_io_handle;
-		using system_file = c_style_file;
-	#elif _POSIX_C_SOURCE
-		using system_io_handle = posix_io_handle;
-		using system_file = posix_file;
-		inline system_io_handle system_out(STDOUT_FILENO);
-		inline fast_io::tie<system_io_handle,system_io_handle> system_in(system_out,STDIN_FILENO);
-		inline fast_io::tie<system_io_handle,system_io_handle> system_err(system_out,STDERR_FILENO);
-	#else
-		using system_io_handle = c_style_io_handle;
-		using system_file = c_style_file;
-	#endif
-	using isystem_file = input_wrapper<system_file>;
-	using osystem_file = output_wrapper<system_file>;
-	using iosystem_file = io_wrapper<system_file>;
+#ifdef _WIN32_WINNT
+	using system_io_handle = c_style_io_handle;
+	using system_file = c_style_file;
+#elif _POSIX_C_SOURCE
+	using system_io_handle = posix_io_handle;
+	using system_file = posix_file;
+	inline system_io_handle system_out(STDOUT_FILENO);
+	inline fast_io::tie<system_io_handle,system_io_handle> system_in(system_out,STDIN_FILENO);
+	inline fast_io::tie<system_io_handle,system_io_handle> system_err(system_out,STDERR_FILENO);
+#else
+	using system_io_handle = c_style_io_handle;
+	using system_file = c_style_file;
+#endif
+using isystem_file = input_wrapper<system_file>;
+using osystem_file = output_wrapper<system_file>;
+using iosystem_file = io_wrapper<system_file>;
 
-	using ibuf = basic_ibuf<isystem_file>;
-	using obuf = basic_obuf<osystem_file>;
-	using iobuf = basic_iobuf<iosystem_file>;
+using ibuf = basic_ibuf<isystem_file>;
+using obuf = basic_obuf<osystem_file>;
+using iobuf = basic_iobuf<iosystem_file>;
 
-	using ibuf_string_view = basic_ibuf_string_view<std::string_view>;
-	using obuf_string = basic_obuf_string<std::string>;
-	using ibuf_mutex = basic_imutex<ibuf>;
-	using obuf_mutex = basic_omutex<obuf>;
-	using iobuf_mutex = basic_iomutex<iobuf>;
-	using ibuf_string_view_mutex = basic_imutex<ibuf_string_view>;
-	using obuf_string_mutex = basic_omutex<obuf_string>;
+using ibuf_string_view = basic_ibuf_string_view<std::string_view>;
+using obuf_string = basic_obuf_string<std::string>;
+using ibuf_mutex = basic_imutex<ibuf>;
+using obuf_mutex = basic_omutex<obuf>;
+using iobuf_mutex = basic_iomutex<iobuf>;
+using ibuf_string_view_mutex = basic_imutex<ibuf_string_view>;
+using obuf_string_mutex = basic_omutex<obuf_string>;
 
 //Due to history reason. Nearly every program is using either iostream or FILE*. We can't break code by using other io handle by default.
-	inline c_style_io_handle out(stdout);
-	inline fast_io::tie<c_style_io_handle,c_style_io_handle> in(out,stdin);
-	inline fast_io::tie<c_style_io_handle,c_style_io_handle> err(out,stderr);
+inline c_style_io_handle out(stdout);
+inline fast_io::tie<c_style_io_handle,c_style_io_handle> in(out,stdin);
+inline fast_io::tie<c_style_io_handle,c_style_io_handle> err(out,stderr);
 
 }
