5a61e3c1cd02e985b9a0a7bf9a04ba04e7f5cfab
ejsvifq_mabmip <euloanty@live.com>
2019-08-24 17:48:11 -0400

    www

---
+++ examples/define_custom_type.cc
@@ -0,0 +1,19 @@
+#include"../include/fast_io.h"
+
+struct foo
+{
+	std::string str;
+};
+
+//fast_io::output_stream is a concept. NOT A TYPE. This is GP not OOP!
+//Define this stream just like define operator<< for iostreams
+fast_io::output_stream& operator<<(fast_io::output_stream& out,foo const& d)
+{
+	return out<<d.str;
+}
+
+int main()
+{
+	foo f{"abc"};
+	fast_io::out<<f<<"\n";
+}
