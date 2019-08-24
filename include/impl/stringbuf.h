c083691dfee5862da97ae51b2b005bd3b3b1789e
ejsvifq_mabmip <euloanty@live.com>
2019-08-24 00:45:46 -0400

    sbf

--- include/impl/stringbuf.h
+++ include/impl/stringbuf.h
@@ -13,6 +13,7 @@ class basic_ibuf_string_view
 public:
 	using value_type = T;
 	using traits_type = typename value_type::traits_type;
+private:
 	using int_type = typename traits_type::int_type;
 	using char_type = typename T::value_type;
 public:
@@ -43,7 +44,7 @@ public:
 			return traits_type::eof();
 		int_type ch(s.front());
 		s.remove_prefix(1);
-		return ch;
+		return traits_type::to_int_type(ch);
 	}
 };
 
@@ -54,46 +55,47 @@ class basic_obuf_string
 public:
 	using value_type = T;
 	using traits_type = typename value_type::traits_type;
+private:
 	using int_type = typename traits_type::int_type;
 	using char_type = typename T::value_type;
+public:
 	template<typename... Args>
-	basic_obuf_string(Args&& ...args):s(std::forward<Args>(args)...){}
-	auto& str()
+	constexpr basic_obuf_string(Args&& ...args):s(std::forward<Args>(args)...){}
+	constexpr auto& str()
 	{
 		return s;
 	}
 	template<typename contiguous_iterator>
-	void write(contiguous_iterator cbegin,contiguous_iterator cend)
+	constexpr void write(contiguous_iterator cbegin,contiguous_iterator cend)
 	{
 		s.append(static_cast<char_type const*>(static_cast<void*>(std::addressof(*cbegin))),static_cast<char_type const*>(static_cast<void*>(std::addressof(*cend))));
 	}
-	template<typename V>
-	void put(V ch)
+	constexpr void put(int_type ch)
 	{
-		s.push_back(ch);
+		s.push_back(traits_type::to_char_type(ch));
 	}
-	void flush(){}
-	void clear(){s.clear();}
+	constexpr void flush(){}
+	constexpr void clear(){s.clear();}
 };
 
 //potential constexpr in the future if std::string can be constexpr
 
 template<typename T=std::string,typename... Args>
-inline T concat(Args&& ...args)
+inline constexpr T concat(Args&& ...args)
 {
 	basic_obuf_string<T> t;
 	print(t,std::forward<Args>(args)...);
 	return std::move(t.str());
 }
 template<typename T=std::string,typename... Args>
-inline T format(std::string_view format,Args&& ...args)
+inline constexpr T format(std::string_view format,Args&& ...args)
 {
 	basic_obuf_string<T> t;
 	fprint(t,format,std::forward<Args>(args)...);
 	return std::move(t.str());
 }
 template<typename T,typename... Args>
-inline void inplace_to(T& t,Args&& ...args)
+inline constexpr void inplace_to(T& t,Args&& ...args)
 {
 	basic_obuf_string<std::string> os;
 	basic_ibuf_string_view<std::string_view> is(print(os,std::forward<Args>(args)...).str());
@@ -101,7 +103,7 @@ inline void inplace_to(T& t,Args&& ...args)
 }
 
 template<typename... Args>
-inline void in_place_to(std::string& t,Args&& ...args)
+inline constexpr void in_place_to(std::string& t,Args&& ...args)
 {
 	basic_obuf_string<std::string> os(std::move(t));
 	os.clear();
@@ -112,7 +114,7 @@ inline void in_place_to(std::string& t,Args&& ...args)
 
 
 template<typename T,typename... Args>
-inline auto to(Args&& ...args)
+inline constexpr auto to(Args&& ...args)
 {
 	T t;
 	in_place_to(t,std::forward<Args>(args)...);
