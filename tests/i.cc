bbe728a651af4bc5015e0de62996cb1854ecc7ab
ejsvifq_mabmip <euloanty@live.com>
2019-08-24 11:45:24 -0400

    222

--- tests/i.cc
+++ tests/i.cc
@@ -1,41 +1,33 @@
 #include"timer.h"
 #include<fstream>
+#include<iostream>
 #include"../include/fast_io.h"
 #include<exception>
 #include<cmath>
 #include<memory>
 #include<cstdio>
-#include"iomutex2.h"
-
-fast_io::obuf_mutex omtx;
-
-void f1()
-{
-	for(std::size_t i(100);i!=200;++i)
-		fprint(omtx,"From f1: %\n",i);
-}
 
 int main()
 try
 {
 	std::size_t constexpr N(10000000);
-/*		{
+	{
 	cqw::timer t("std::FILE*");
 	std::unique_ptr<std::FILE,decltype(fclose)*> fp(std::fopen("cfilestar.txt","wb"),fclose);
 	for(std::size_t i(0);i!=N;++i)
 		fprintf(fp.get(),"%zu\n",i);
-	}*/
-/*	{
+	}
+	{
 	cqw::timer t("obuf");
-	fast_io::obuf obuf("obuf.txt");
+	fast_io::obuf obuf("obuf.txt",fast_io::open::interface<fast_io::open::binary>);
 	for(std::size_t i(0);i!=N;++i)
 		(obuf<<i).put('\n');
-	}*/
+	}
 	{
-	cqw::timer t("obuf_fprint");
-	fast_io::obuf obuf("obuf_fprint.txt");
+	cqw::timer t("hacked c_style file");
+	fast_io::c_style_file csf("hack.txt",fast_io::open::interface<fast_io::open::out|fast_io::open::binary>);
 	for(std::size_t i(0);i!=N;++i)
-		fprint(obuf,"%\n",i);
+		(csf<<i).put('\n');
 	}
 /*	{
 	cqw::timer t("obuf_mutex");
@@ -46,7 +38,5 @@ try
 }
 catch(std::exception const& e)
 {
-	fast_io::log lg;
-	print(lg,e.what(),"\n");
 	return 1;
 }
