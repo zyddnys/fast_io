b9f17654ce875a60eeddb6628fb8cf89d8b21b42
ejsvifq_mabmip <euloanty@live.com>
2019-08-24 18:19:16 -0400

    q

--- examples/a+b.cc
+++ examples/a+b.cc
@@ -5,8 +5,6 @@ int main()
 {
 	std::size_t a,b;
 	print(fast_io::out,"Please input 2 numbers\n");
-	fast_io::in>>a>>b;
+	scan(fast_io::in,a,b);
 	fprint(fast_io::out,"sum of a+b = %\n",a+b);
-	std::size_t c;
-
 }
