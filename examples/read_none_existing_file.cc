78cb16c2324544812d8b3e9f54ab873692792a78
ejsvifq_mabmip <euloanty@live.com>
2019-08-24 17:44:23 -0400

    q

--- examples/read_none_existing_file.cc
+++ examples/read_none_existing_file.cc
@@ -1,18 +1,12 @@
-#include<exception>
 #include"../include/fast_io.h"
-#include"../include/impl/tie.h"
-
-#include<iostream>
 
 int main()
 try
 {
-	fast_io::obuf ob("tied.txt");
-	fast_io::tie<fast_io::ibuf,fast_io::obuf> ib(ob,"not_existing_file.txt");
-	std::cout<<fast_io::output_stream<decltype(fast_io::tie<fast_io::ibuf,fast_io::obuf>)>()<<'\n';
+	fast_io::ibuf ib("not_existing_file.txt");
 }
 catch(std::exception const& e)
 {
-	std::cerr<<e.what()<<'\n';
+	fprint(fast_io::err,"%\n",e.what());
 	return 1;
 }
