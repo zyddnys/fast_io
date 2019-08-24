6711c7138b66bb1f0867e0cfc1c6dcafc9e00333
ejsvifq_mabmip <euloanty@live.com>
2019-08-24 17:36:17 -0400

    q

--- examples/tie.cc
+++ examples/tie.cc
@@ -1,17 +1,18 @@
 #include"../include/fast_io.h"
 #include"../include/impl/tie.h"
 #include<exception>
-#include<iostream>
 
 int main()
 try
 {
 	fast_io::obuf ob("tied.txt");
 	fast_io::tie<fast_io::ibuf,fast_io::obuf> ib(ob,"not_existing_file.txt");
-	std::cout<<fast_io::output_stream<decltype(fast_io::tie<fast_io::ibuf,fast_io::obuf>)>()<<'\n';
+	ob<<"35\n";
+	std::size_t d;
+	ib>>d;
 }
 catch(std::exception const& e)
 {
-	std::cerr<<e.what()<<'\n';
+	fprint(fast_io::err,"%\n",e);
 	return 1;
 }
