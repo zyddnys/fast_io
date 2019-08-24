23270e0c7305f886e8de2dcf1d5e3047cee1d8c9
ejsvifq_mabmip <euloanty@live.com>
2019-08-24 13:52:56 -0400

    qqq

--- include/impl/handlers/c_style.h
+++ include/impl/handlers/c_style.h
@@ -3,51 +3,30 @@
 #include<cstddef>
 #include"../mode.h"
 #include<system_error>
-#include"exploiting_file_ptr.h"
+//#include"exploiting_file_ptr.h"
 
 namespace fast_io
 {
 
-class c_style_file
+class c_style_io_handle
 {
 	std::FILE *fp;
-	void close_impl() noexcept
+protected:
+	native_handle_type& protected_native_handle()
 	{
-		if(fp)
-			std::fclose(fp);
+		return fp;
 	}
 public:
+	c_style_io_handle(std::FILE* fpp):fp(fpp){}
 	using traits_type = std::char_traits<char>;
 	using native_handle_type = std::FILE*;
-	template<typename ...Args>
-	c_style_file(native_interface_t,Args&& ...args):fp(std::fopen(std::forward<Args>(args)...))
-	{
-		if(fp==nullptr)
-			throw std::system_error(errno,std::generic_category());
-	}
-	c_style_file(std::string_view name,std::string_view mode):c_style_file(native_interface,name.data(),mode.data()){}
-	c_style_file(std::string_view file,open::mode const& m):c_style_file(file,c_style(m)){}
-	template<std::size_t om>
-	c_style_file(std::string_view name,open::interface_t<om>):c_style_file(name,open::interface_t<om>::c_style){}
-	c_style_file(c_style_file const&)=delete;
-	c_style_file& operator=(c_style_file const&)=delete;
-	c_style_file(c_style_file&& b) noexcept : fp(b.fp)
-	{
-		b.fp = nullptr;
-	}
-	c_style_file& operator=(c_style_file&& b) noexcept
-	{
-		if(&b!=this)
-		{
-			close_impl();
-			fp=b.fp;
-			b.fp = nullptr;
-		}
-		return *this;
-	}
 	native_handle_type native_handle() const
 	{
 		return fp;
+	}
+	bool eof() const
+	{
+		return std::feof(fp);
 	}
 	template<typename ContiguousIterator>
 	ContiguousIterator read(ContiguousIterator begin,ContiguousIterator end)
@@ -58,14 +37,7 @@ public:
 			return begin+r;
 		throw std::system_error(errno,std::generic_category());
 	}
-	bool eof() const
-	{
-		return std::feof(fp);
-	}
-	operator bool() const
-	{
-		return !eof();
-	}
+
 	template<typename ContiguousIterator>
 	void write(ContiguousIterator begin,ContiguousIterator end)
 	{
@@ -79,9 +51,8 @@ public:
 	}
 	void put(traits_type::int_type ch)
 	{
-		hacking::filestar_fast_put(fp,ch);
-//		if(fputc(ch,fp)==EOF)
-//			throw std::system_error(errno,std::system_category());
+		if(fputc(ch,fp)==EOF)
+			throw std::system_error(errno,std::system_category());
 	}
 	template<typename ...Args>
 	void printf(Args&& ...args)
@@ -93,6 +64,44 @@ public:
 	{
 		if(std::fflush(fp))
 			throw std::system_error(errno,std::system_category());
+	}
+};
+
+class c_style_file:public c_style_io_handle
+{
+	void close_impl() noexcept
+	{
+		if(native_handle())
+			std::fclose(native_handle());
+	}
+public:
+	using traits_type = std::char_traits<char>;
+	using native_handle_type = std::FILE*;
+	template<typename ...Args>
+	c_style_file(native_interface_t,Args&& ...args):c_style_io_handle(std::fopen(std::forward<Args>(args)...))
+	{
+		if(native_handle()==nullptr)
+			throw std::system_error(errno,std::generic_category());
+	}
+	c_style_file(std::string_view name,std::string_view mode):c_style_file(native_interface,name.data(),mode.data()){}
+	c_style_file(std::string_view file,open::mode const& m):c_style_file(file,c_style(m)){}
+	template<std::size_t om>
+	c_style_file(std::string_view name,open::interface_t<om>):c_style_file(name,open::interface_t<om>::c_style){}
+	c_style_file(c_style_file const&)=delete;
+	c_style_file& operator=(c_style_file const&)=delete;
+	c_style_file(c_style_file&& b) noexcept : c_style_io_handle(b.native_handle())
+	{
+		b.protected_native_handle() = nullptr;
+	}
+	c_style_file& operator=(c_style_file&& b) noexcept
+	{
+		if(&b!=this)
+		{
+			close_impl();
+			protected_native_handle()=b.native_handle();
+			b.protected_native_handle() = nullptr;
+		}
+		return *this;
 	}
 	~c_style_file()
 	{
