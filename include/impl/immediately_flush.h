1221ad3d4cadb05ec2e4cb41a8c857d99de5dec5
ejsvifq_mabmip <euloanty@live.com>
2019-08-25 16:52:08 -0400

    s

---
+++ include/impl/immediately_flush.h
@@ -0,0 +1,35 @@
+#pragma once
+
+namespace fast_io
+{
+template<output_stream Ohandler>
+class immediately_flush
+{
+	Ohandler oh;
+public:
+	using native_handle_type = Ohandler;
+	using char_type = typename native_handle_type::char_type;
+	template<typename... Args>
+	constexpr immediately_flush(Args&&... args):oh(std::forward<Args>(args)...){bh.curr=bh.beg;}
+	constexpr void flush()
+	{
+		oh.flush();
+	}
+	template<typename Contiguous_Iterator>
+	constexpr void write(Contiguous_Iterator cbegin,Contiguous_Iterator cend)
+	{
+		oh.write(cbegin,cend);
+		oh.flush();
+	}
+	constexpr void put(char_type ch)
+	{
+		auto const address(std::addressof(ch));
+		oh.write(address,address+1);
+		oh.flush();
+	}
+	constexpr auto& native_handle()
+	{
+		return oh;
+	}
+};
+}
