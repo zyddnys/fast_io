782afb1c783728096a455247a9f0fcc4bfa2de00
ejsvifq_mabmip <euloanty@live.com>
2019-08-25 03:35:48 -0400

    ????

---
+++ examples/unicode.cc
@@ -0,0 +1,7 @@
+#include"../include/fast_io.h"
+int main()
+{
+	fast_io::obuf ob("unicode.txt");
+	fast_io::unicode_view<decltype(ob),wchar_t> uv(ob);
+	uv<<L"工要在地一，上是中国同和的有人我主产不为这经以发了民㍿dsgds\n";
+}
