a26aaca509baea90aa37f5069858e82002f8311f
ejsvifq_mabmip <euloanty@live.com>
2019-08-25 04:24:40 -0400

    s

--- examples/unicode.cc
+++ examples/unicode.cc
@@ -1,6 +1,7 @@
 #include"../include/fast_io.h"
 int main()
 {
-	using namespace std::literals;
-	fast_io::out<<fast_io::unicode_to_utf8(L"工要在地一，上是中国同和的有人我主产不为这经以发了民㍿dsgds\n"sv);
+	fast_io::obuf ob("unicode.txt");
+	fast_io::unicode_view<decltype(ob),wchar_t> uv(ob);
+	uv<<L"工要在地一，上是中国同和的有人我主产不为这经以发了民㍿dsgds\n";
 }
