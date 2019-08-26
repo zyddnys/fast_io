cdcfec4fad5416ec96247a99d1becc84f3e1ab17
ejsvifq_mabmip <euloanty@live.com>
2019-08-25 22:40:57 -0400

    w

--- examples/0013.incorrect examples/outputline.cc
+++ examples/0013.incorrect examples/outputline.cc
@@ -2,7 +2,7 @@
 
 int main()
 {
-	fast_io::out<<3<<'\n';//WRONG. character is treated in fast_io library as integer by default. You can't do so.
+	fast_io::out<<3<<'\n';//WRONG. character is treated in fast_io library as integer by default. '\n' would be treated as its ANSI value
 // The reason is that std::int8_t/std::uint8_t would be treated as integer in iostream which breaks a lot of generic programming code. I decide to NEVER support shit like this.
 
 /*
