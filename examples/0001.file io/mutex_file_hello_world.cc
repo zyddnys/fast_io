0e934e5ae182c1d52e9f64de7e02412b220374d8
ejsvifq_mabmip <euloanty@live.com>
2019-08-25 21:23:08 -0400

    w

--- examples/0001.file io/mutex_file_hello_world.cc
+++
@@ -1,7 +0,0 @@
-#include"../../include/fast_io.h"
-
-int main()
-{
-	fast_io::obuf_mutex obuf("file_hello_world.txt");// will automatically lock after each io operation. lock itself is exception safe
-	obuf<<"Hello World!\n";
-}
