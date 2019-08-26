16eaa8b5be716d2f2fb513ed375a08b59a132bf1
ejsvifq_mabmip <euloanty@live.com>
2019-08-25 23:58:13 -0400

    www

---
+++ examples/0004.convert/size_t_to_string_ostring_directly.cc
@@ -0,0 +1,10 @@
+#include"../../include/fast_io.h"
+
+int main()
+{
+	std::size_t sz(23623643);
+	fast_io::ostring os;
+	os<<sz;
+	std::string str(std::move(os.str()));			//YES. You can move string from ostring!
+	println(fast_io::out,"convert from std::size_t 23623643: ",str);
+}
