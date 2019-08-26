85a3c960487dd81b7ab4f788359a62c08da2813e
ejsvifq_mabmip <euloanty@live.com>
2019-08-25 20:15:53 -0400

    w

--- examples/read_none_existing_file.cc
+++ examples/read_none_existing_file.cc
@@ -1,12 +1,6 @@
 #include"../include/fast_io.h"
 
 int main()
-try
 {
 	fast_io::ibuf ib("not_existing_file.txt");
-}
-catch(std::exception const& e)
-{
-	fprint(fast_io::err,"%\n",e.what());
-	return 1;
 }
