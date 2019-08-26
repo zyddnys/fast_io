25f03c9404fe8fa12339c15cae5e521609ca45cf
ejsvifq_mabmip <euloanty@live.com>
2019-08-25 22:50:35 -0400

    w

--- examples/0013.incorrect examples/eof_correct.cc
+++ examples/0013.incorrect examples/eof_correct.cc
@@ -5,7 +5,7 @@ int main()
 {
 	std::string str;
 	for(std::pair<char,bool> ch;!((ch=fast_io::in.try_get()).second);str.push_back(ch));
-	
+
 /*
 try_get() returns 2 value. bool represents whether it reaches EOF. true means it reaches EOF.
 When it reaches EOF. the first member of the pair is guarantee to be 0.
