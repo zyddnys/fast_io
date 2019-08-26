9127a2ce4a6751824e8c969f7c5977e06b4c8a75
ejsvifq_mabmip <euloanty@live.com>
2019-08-25 23:04:55 -0400

    w

--- examples/0001.file io/open_none_existing_file.cc
+++ examples/0001.file io/open_none_existing_file.cc
@@ -1,6 +1,13 @@
 #include"../../include/fast_io.h"
+#include<exception>
 
 int main()
+try
 {
 	fast_io::ibuf ib("not_existing_file.txt");
+}
+catch(std::exception const &e)
+{
+	fast_io::err<<e<<"\n";
+	return 1;
 }
