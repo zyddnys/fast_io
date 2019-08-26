be4cf7c8599695f1c099332237f1594f9ea2468a
ejsvifq_mabmip <euloanty@live.com>
2019-08-26 06:18:43 -0400

    ww

---
+++ examples/0018.base/hexa+b.cc
@@ -0,0 +1,11 @@
+#include"../../include/fast_io.h"
+#include<cstddef>
+
+int main()
+{
+	fast_io::out<<"Please input 2 hex numbers\n";
+	std::size_t a,b;
+	fast_io::in>>fast_io::hex(a)>>fast_io::hex(b);
+	fprint(fast_io::out,"sum of %(hex:%)+%(hex:%) = %(hex:%)\n",a,fast_io::hex(a),b,fast_io::hex(b)
+						,a+b,fast_io::hex(a+b));
+}
