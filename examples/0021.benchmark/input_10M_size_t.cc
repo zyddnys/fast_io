4585cb01fcdf754542a5d986487be4f0c3bf4260
ejsvifq_mabmip <euloanty@live.com>
2019-08-27 03:08:13 -0400

    ww

---
+++ examples/0021.benchmark/input_10M_size_t.cc
@@ -0,0 +1,52 @@
+//Please run output_10M_size_t before this
+
+#include"timer.h"
+#include<fstream>
+#include"../../include/fast_io.h"
+#include<exception>
+#include<memory>
+#include<cstdio>
+#include<vector>
+
+int main()
+try
+{
+	std::size_t constexpr N(10000000);
+	std::vector<std::size_t> v(N);
+	{
+	cqw::timer t("std::FILE*");
+	std::unique_ptr<std::FILE,decltype(fclose)*> fp(std::fopen("cfilestar.txt","rb"),fclose);
+	for(std::size_t i(0);i!=N;++i)
+		fscanf(fp.get(),"%zu\n",v.data()+i);
+	}
+	{
+	cqw::timer t("std::ifstream");
+	std::ifstream fin("cfilestar.txt",std::ofstream::binary);
+	for(std::size_t i(0);i!=N;++i)
+		fin>>v[i];
+	}
+	{
+	cqw::timer t("ibuf");
+	fast_io::ibuf ibuf("cfilestar.txt",fast_io::open::interface<fast_io::open::binary>);
+	for(std::size_t i(0);i!=N;++i)
+		ibuf>>v[i];
+	}
+	{
+	cqw::timer t("dynamic standard input stream ibuf");
+	fast_io::dynamic_standard_input_stream ibuf(std::in_place_type<fast_io::ibuf>,"cfilestar.txt",fast_io::open::interface<fast_io::open::binary>);
+	for(std::size_t i(0);i!=N;++i)
+		ibuf>>v[i];
+	}
+	{
+	std::vector<std::size_t> v1(N);
+	cqw::timer t("ibuf_dynamic isystem_file");
+	fast_io::ibuf_dynamic ibuf(std::in_place_type<fast_io::isystem_file>,"cfilestar.txt",fast_io::open::interface<fast_io::open::binary>);
+	for(std::size_t i(0);i!=N;++i)
+		ibuf>>v1[i];
+	}
+}
+catch(std::exception const& e)
+{
+	fast_io::err<<e<<"\n";
+	return 1;
+}
---
+++ examples/0021.benchmark/output_10M_size_t.cc
@@ -0,0 +1,65 @@
+#include"timer.h"
+#include<fstream>
+#include"../../include/fast_io.h"
+#include<exception>
+#include<cmath>
+#include<memory>
+#include<cstdio>
+
+int main()
+try
+{
+	std::size_t constexpr N(10000000);
+	{
+	cqw::timer t("std::FILE*");
+	std::unique_ptr<std::FILE,decltype(fclose)*> fp(std::fopen("cfilestar.txt","wb"),fclose);
+	for(std::size_t i(0);i!=N;++i)
+		fprintf(fp.get(),"%zu\n",i);
+	}
+	{
+	cqw::timer t("std::ofstream");
+	std::ofstream fout("ofstream.txt",std::ofstream::binary);
+	for(std::size_t i(0);i!=N;++i)
+		fout<<i<<'\n';
+	}
+	{
+	cqw::timer t("std::ofstream with tricks");
+	std::ofstream fout("ofstream_tricks.txt",std::ofstream::binary);
+	auto &rdbuf(*fout.rdbuf());
+	for(std::size_t i(0);i!=N;++i)
+	{
+		fout<<i;
+		rdbuf.sputc('\n');
+	}
+	}
+	{
+	cqw::timer t("obuf");
+	fast_io::obuf obuf("obuf.txt",fast_io::open::interface<fast_io::open::binary>);
+	for(std::size_t i(0);i!=N;++i)
+		println(obuf,i);
+	}
+	{
+	cqw::timer t("dynamic obuf");
+	fast_io::dynamic_standard_output_stream dobuf(std::in_place_type<fast_io::obuf>,"dynamic_obuf.txt",fast_io::open::interface<fast_io::open::binary>);
+	for(std::size_t i(0);i!=N;++i)
+		println(dobuf,i);
+	}
+	{
+	cqw::timer t("iobuf_dynamic system_file");
+	fast_io::iobuf_dynamic dobuf(std::in_place_type<fast_io::osystem_file>,"iobuf_dynamic_system_file.txt",fast_io::open::interface<fast_io::open::binary>);
+	for(std::size_t i(0);i!=N;++i)
+		println(dobuf,i);
+	}
+	{
+	cqw::timer t("obuf_mutex");
+	fast_io::obuf_mutex obuf("obuf_mutex.txt",fast_io::open::interface<fast_io::open::binary>);
+	for(std::size_t i(0);i!=N;++i)
+		println(obuf,i);
+	}
+	
+}
+catch(std::exception const& e)
+{
+	println(fast_io::err,e);
+	return 1;
+}
