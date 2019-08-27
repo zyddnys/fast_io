d311f6bcd8669dfd1a87b7fad9f8a2013b1cc839
ejsvifq_mabmip <euloanty@live.com>
2019-08-27 19:39:11 -0400

    multithreads

---
+++ examples/0022.mutex/multithreads.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+#include"../../include/fast_io.h"
+#include<thread>
+
+fast_io::obuf_mutex file_log("log.txt");
+
+void thread_function1()
+{
+	for(std::size_t i(0);i!=10000;++i)
+		println(file_log,"thread1:",i);
+}
+
+void thread_function2()
+{
+	for(std::size_t i(0);i!=10000;++i)
+		println(file_log,"thread2:",i);
+}
+
+int main()
+{
+	std::thread th1(thread_function1);
+	std::thread th2(thread_function2);
+	for(std::size_t i(0);i!=10000;++i)
+		println(file_log,"main thread:",i);
+	th1.join();
+	th2.join();
+}
