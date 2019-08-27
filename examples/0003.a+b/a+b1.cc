6ca514ba36a8be1723d1e0a16632d19b03184006
ejsvifq_mabmip <euloanty@live.com>
2019-08-27 02:12:44 -0400

    w

---
+++ examples/0003.a+b/a+b1.cc
@@ -0,0 +1,10 @@
+#include"../../include/fast_io.h"
+#include<cstddef>
+
+int main()
+{
+	fast_io::out<<"Please input 2 numbers\n";
+	std::size_t a,b;
+	scan(fast_io::in,a,b);
+	println(fast_io::out,"sum of a+b = ",a+b);
+}
