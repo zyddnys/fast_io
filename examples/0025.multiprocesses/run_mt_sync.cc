0f4bada1aba26a6c52048427b663d12e312165d6
ejsvifq_mabmip <euloanty@live.com>
2019-08-28 17:38:00 -0400

    run_mt runs its own threads for background

--- examples/0025.multiprocesses/run_mt_sync.cc
+++ examples/0025.multiprocesses/run_mt_sync.cc
@@ -1,8 +1,18 @@
 #include"../../include/fast_io.h"
 #include<cstdlib>
+#include<thread>
+#include<vector>
+
+void launch_process(std::size_t process_number)
+{
+	std::system(fast_io::concat<>(".\\process_number ",process_number).c_str());
+}
 
 int main()
 {
+	std::vector<std::thread> vecs;
 	for(std::size_t i(0);i!=100;++i)
-		std::system(fast_io::concat<>("./multithread_sync ",i," &").c_str());
+		vecs.emplace_back(launch_process,i);
+	for(auto & e : vecs)
+		e.join();
 }
