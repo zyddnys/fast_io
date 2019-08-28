beb768ab2f1aecba7e0a2712a6329b1fbf2c8b9f
ejsvifq_mabmip <euloanty@live.com>
2019-08-28 14:38:55 -0400

    wrapper should not with in tag by default

--- include/impl/wrapper.h
+++ include/impl/wrapper.h
@@ -15,7 +15,7 @@ public:
 	constexpr basic_wrapper(native_interface_t,Args&& ...args):T(std::forward<Args>(args)...){}
 	template<std::size_t om>
 	constexpr basic_wrapper(std::string_view file,open::interface_t<om>):T(file,open::interface<fast_io::open::mode(om|interface_mode)>){}
-	constexpr basic_wrapper(std::string_view file):T(file,open::interface<fast_io::open::in|fast_io::open::mode(interface_mode)>){}
+	constexpr basic_wrapper(std::string_view file):T(file,open::interface<fast_io::open::mode(interface_mode)>){}
 	constexpr basic_wrapper(std::string_view file,open::mode const& m):T(file,m|fast_io::open::mode(interface_mode)){}
 	constexpr basic_wrapper(std::string_view file,std::string_view mode):basic_wrapper(file,fast_io::open::c_style(mode)){}
 };
