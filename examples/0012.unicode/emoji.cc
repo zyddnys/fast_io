cd42fbc480bfd57f70173eb3a4e48a426fd956c4
ejsvifq_mabmip <euloanty@live.com>
2019-08-29 05:01:52 -0400

    emoji!!!

--- examples/0012.unicode/emoji.cc
+++ examples/0012.unicode/emoji.cc
@@ -5,6 +5,7 @@ int main()
 	using namespace fast_io;
 	fast_io::obuf ob("emoji.txt");
 	std::string_view const emoji(u8"☭🚄😀我wubi😄😊😉😍😘😚😜😝😳😁😣😢😂😭😪😥😰😩㍿🀀🀁🀂🀃🀄🀅🀆🀇🀈🀉🀊🀋🀌🀍🀎🀏");
+	println(ob,"original emoji string:\n",emoji);
 	for(auto const & e : emoji)
 		print(ob,unsigned_view(e)," ");
 	print(ob,"\n\n");
@@ -17,7 +18,8 @@ int main()
 	}
 	println(ob);
 	auto const convert_back(unicode_to_utf8(u32emoji));
+	println(ob,"convert back emoji string:\n",emoji);
 	for(auto const& e : convert_back)
 		print(ob,unsigned_view(e)," ");
-	println(ob,"\nSame?\n",emoji==convert_back);
+	println(ob,"\n\nSame?\n",emoji==convert_back);
 }
