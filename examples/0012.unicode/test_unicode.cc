0052cf20795b38be387a52750427e64394bb4f04
ejsvifq_mabmip <euloanty@live.com>
2019-08-29 05:12:20 -0400

    test unicode

---
+++ examples/0012.unicode/test_unicode.cc
@@ -0,0 +1,12 @@
+#include"../../include/fast_io.h"
+
+int main()
+{
+	using namespace fast_io;
+	fast_io::obuf ob("test_unicode.txt");
+	for(char32_t i(0);i!=1000000;++i)
+	{
+		std::u32string str(1,i);
+		assert(utf8_to_unicode<std::u32string>(unicode_to_utf8(str))==str);
+	}
+}
