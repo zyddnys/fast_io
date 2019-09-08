43aaaee0ec99eaba8abeafd5f4dc7ade0a558962
ejsvifq_mabmip <euloanty@live.com>
2019-09-08 05:53:49 -0400

    scan int until eof

---
+++ examples/0032.eof/scan_int_until_eof.cc
@@ -0,0 +1,25 @@
+#include"../../include/fast_io.h"
+#include<vector>
+
+int main()
+try
+{
+	fast_io::ibuf ib("in.txt");
+	std::vector<std::size_t> vec;
+	try
+	{
+		for(;;)
+		{
+			std::size_t a;
+			scan(ib,a);
+			vec.emplace_back(a);
+		}
+	}
+	catch(fast_io::eof const&){}
+	println(fast_io::out,vec.size());
+}
+catch(std::exception const &e)
+{
+	println(fast_io::err,e);
+	return 1;
+}
