561b0014a87e50f2f5bf1031fa63eadfda611973
ejsvifq_mabmip <euloanty@live.com>
2019-09-09 01:51:52 -0400

    self tie

--- include/impl/tie.h
+++ include/impl/tie.h
@@ -9,8 +9,8 @@ class tie
 {
 	out& o;
 public:
-	using native_interface_t = T;
-	using char_type = typename native_interface_t::char_type;
+	using native_handle_type = T;
+	using char_type = typename native_handle_type::char_type;
 private:
 	T t;
 public:
@@ -49,6 +49,55 @@ public:
 	{
 		o.flush();
 		return t.write(begin,end);
+	}
+};
+
+template<io_stream T>
+class self_tie
+{
+public:
+	using native_handle_type = T;
+	using char_type = typename native_handle_type::char_type;
+private:
+	T t;
+public:
+	template<typename ...Args>
+	constexpr self_tie(Args&& ...args):t(std::forward<Args>(args)...){}
+	constexpr auto& native_handle() {return t;}
+	constexpr auto get() requires standard_input_stream<T>
+	{
+		t.flush();
+		return t.get();
+	}
+	constexpr auto try_get() requires standard_input_stream<T>
+	{
+		t.flush();
+		return t.try_get();
+	}
+	constexpr void put(char_type ch) requires standard_output_stream<T>
+	{
+		return t.put(ch);
+	}
+	constexpr void flush() requires output_stream<T>
+	{
+		t.flush();
+	}
+	template<typename Contiguous_iterator>
+	constexpr Contiguous_iterator read(Contiguous_iterator begin,Contiguous_iterator end) 	requires input_stream<T>
+	{
+		t.flush();
+		return t.read(begin,end);
+	}
+	template<typename Contiguous_iterator>
+	constexpr void write(Contiguous_iterator begin,Contiguous_iterator end) requires output_stream<T>
+	{
+		return t.write(begin,end);
+	}
+	template<typename... Args>
+	void seek(Args&& ...args) requires random_access_stream<native_handle_type>
+	{
+		t.flush();
+		t.seek(std::forward<Args>(args)...);
 	}
 };
 
