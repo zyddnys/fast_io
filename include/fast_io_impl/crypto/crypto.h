e5f802bd32e98eba50ac907f8edd8e8bcb209355
ejsvifq_mabmip <euloanty@live.com>
2019-11-01 17:46:53 -0400

    crysimple

--- include/fast_io_impl/crypto/crypto.h
+++ include/fast_io_impl/crypto/crypto.h
@@ -6,3 +6,20 @@
 #include "ctr.h"
 #include "speck.h"
 //#include "aes.h"
+
+namespace fast_io::crypto
+{
+
+template<input_stream in>
+inline ictr_speck=basic_ictr<in,speck::speck_enc_128_256>;
+
+template<output_stream out>
+inline octr_speck=basic_octr<out,speck::speck_enc_128_256>;
+
+template<input_stream in>
+inline icbc_speck=basic_ictr<in,speck::speck_dec_128_256>;
+
+template<output_stream out>
+inline ocbc_speck=basic_octr<out,speck::speck_enc_128_256>;
+
+}
